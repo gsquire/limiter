26f08a9c102418c4ac5f8597ed918a352cd06671
Garrett Squire <garrettsquire@gmail.com>
2016-05-24 13:50:52 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# limiter
+
+This is an example of `BeforeMiddleware` for the [Iron](https://github.com/iron/iron) framework.  It limits the request body size
+by checking first the Content-Length header, then the size of the payload. The response is either
+an HTTP 413 or it continues down the chain.
+
+Include this in your `Cargo.toml` file:
+
+```sh
+[dependencies]
+limiter = "0.1.0"
+```
+
+### Example
+
+```rust
+extern crate iron;
+extern crate limiter;
+
+use iron::prelude::*;
+
+use limiter::BodyLimit;
+
+fn index(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((iron::status::Ok, "Test")))
+}
+
+fn main() {
+    let max_request = BodyLimit::new(5000);
+    let mut chain = Chain::new(index);
+    chain.link_before(max_request);
+    Iron::new(chain).http("localhost:3000").unwrap();
+}
+```
+
+### License
+MIT
---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+extern crate iron;
+
+use iron::BeforeMiddleware;
+use iron::headers::ContentLength;
+use iron::prelude::*;
+use iron::status;
+
+use std::error::Error;
+use std::fmt;
+use std::io::Read;
+
+// The default is 20 MB.
+const DEFAULT_LIMIT: u64 = 2e7 as u64;
+
+/// The error thrown when the request body size is larger than the limit set
+/// by the middleware.
+#[derive(Debug)]
+pub struct RequestTooLarge;
+
+pub struct BodyLimit {
+    max_body: u64,
+}
+
+impl fmt::Display for RequestTooLarge {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("Request body too large.")
+    }
+}
+
+impl Error for RequestTooLarge {
+    fn description(&self) -> &str {
+        "Request too large"
+    }
+}
+
+impl BodyLimit {
+    /// Construct a new request body size limiter for Iron.
+    pub fn new(max: u64) -> BodyLimit {
+        if max <= 0 {
+            return BodyLimit { max_body: DEFAULT_LIMIT };
+        }
+        BodyLimit { max_body: max }
+    }
+
+    // Set the proper error response if the request body is too big or carry on
+    // down the chain.
+    fn check_octets(&self, total: u64) -> IronResult<()> {
+        if total > self.max_body {
+            Err(IronError::new(RequestTooLarge, status::PayloadTooLarge))
+        } else {
+            Ok(())
+        }
+    }
+}
+
+impl BeforeMiddleware for BodyLimit {
+    // This middleware tries to read the content length of the request first
+    // before reading the request body.
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        let length = req.headers.get::<ContentLength>();
+        match length {
+            Some(l) => { self.check_octets(l.0) },
+            // The only way this works is if the transfer-encoding is chunked.
+            // Otherwise the hyper library returns an empty reader which says it
+            // read zero bytes. See the HttpReader type in the hyper documentation.
+            None => {
+                let mut buf = Vec::new();
+                let read_ok = req.body.read_to_end(&mut buf);
+                if read_ok.is_ok() {
+                    self.check_octets(read_ok.unwrap() as u64)
+                } else {
+                    Err(IronError::new(read_ok.err().unwrap(), status::InternalServerError))
+                }
+            }
+        }
+    }
+}
